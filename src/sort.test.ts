a70d1025f065dce25f9d0aa10beb74e2d358052d
Omochice <mochice.mls.ntl@gmail.com>
2023-04-27 19:47:15 +0900

    :herb: Add test for tsort

---
+++ src/sort.test.ts
@@ -0,0 +1,53 @@
+import { assert } from "https://deno.land/std@0.184.0/testing/asserts.ts";
+import { topologicalSort } from "./sort.ts";
+
+Deno.test("topologicalSort", async (t) => {
+  await t.step("All of tasks are isolated, It will be by the order", () => {
+    const tasks = [
+      { to: [], expect: 0 },
+      { to: [], expect: 1 },
+      { to: [], expect: 2 },
+    ];
+    assert(topologicalSort(tasks).map((v, i) => v.expect === i));
+  });
+
+  await t.step("Depend", () => {
+    const tasks = [
+      { to: [], expect: 2 },
+      { to: [0], expect: 1 },
+      { to: [1], expect: 0 },
+    ];
+    assert(topologicalSort(tasks).map((v, i) => v.expect === i));
+  });
+
+  await t.step("Include circly, will remove their", () => {
+    const tasks = [
+      { to: [2], expect: undefined },
+      { to: [0], expect: undefined },
+      { to: [1], expect: undefined },
+      { to: [], expect: 0 },
+    ];
+    assert(
+      topologicalSort(tasks).map((v, i) =>
+        v.expect !== undefined && v.expect === i
+      ),
+    );
+  });
+
+  await t.step("Forked", () => {
+    const tasks = [
+      { to: [1, 2], expect: 0 },
+      { to: [3], expect: [1, 2] },
+      { to: [3], expect: [1, 2] },
+      { to: [], expect: [3] },
+    ];
+    assert(
+      topologicalSort(tasks).map((v, i) => {
+        if (Array.isArray(v.expect)) {
+          return v.expect.includes(i);
+        }
+        v.expect === i;
+      }),
+    );
+  });
+});
